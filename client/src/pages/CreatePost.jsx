e44aea1824f8f4d268cf74c954e6cd18643837c0
Sid Shelke <ausv2027@gmail.com>
2023-02-16 16:25:29 +0530

    first commit

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,172 @@
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom"
+
+import { preview } from "../assets"
+import { getRandomPrompt } from "../utils"
+import { FormField, Loader } from "../components"
+
+const CreatePost = () => {
+
+  const navigate = useNavigate()
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: ""
+  })
+
+  const [genratingImg, setGenratingImg] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGenratingImg(true)
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ prompt: form.prompt })
+
+        })
+
+        const data = await response.json()
+
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` })
+      } catch (error) {
+        alert(error)
+
+      } finally {
+        setGenratingImg(false)
+      }
+    } else {
+      alert("Please enter a prompt")
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
+
+        await response.json();
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
+  };
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt)
+    setForm({ ...form, prompt: randomPrompt })
+  }
+
+  return (
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px] ">
+          Create</h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>
+          Create imaginative and visually stunning images through DALL-E AI
+        </p>
+      </div>
+      <div>
+        <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+          <div className='flex flex-col gap-5'>
+            <FormField
+              LabelName="Your Name"
+              type="text"
+              name="name"
+              placeholder="John Doe"
+              value={form.name}
+              handleChange={handleChange}
+            />
+            <FormField
+              LabelName="Prompt"
+              type="text"
+              name="prompt"
+              placeholder="A 3D render of a rainbow colored hot air balloon flying above a reflective lake"
+              value={form.prompt}
+              handleChange={handleChange}
+              isSurpriseMe
+              handleSurpriseMe={handleSurpriseMe}
+            />
+            <div className='relative bg-gray-50 border border-gray-300
+             text-gray-900 text-sm rounded-lg
+             focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64
+              flex justify-center items-center'>
+              {form.photo ? (
+                <img
+
+                  src={form.photo}
+                  alt={form.prompt}
+                  className="w-full h-full object-contain"
+                />) : (
+                <img
+                  src={preview}
+                  alt="preview"
+                  className="w-9/12 h-9/12 
+                  object-contain opacity-40"
+                />
+              )
+              }
+              {genratingImg && (
+                <div className='absolute inset-0 z-0 flex justify-center
+               items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                  <Loader />
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className='mt-5 flex gap-5'>
+            <button
+              type="button"
+              onClick={generateImage}
+              className=" text-white bg-green-700 font-medium rounded-md text-sm
+                   w-full  px-5 py-2.5 sm:w-auto text-center"
+            >
+              {genratingImg ? "Generating..." : "Generate Image"}
+
+            </button>
+          </div>
+          <div className='mt-10'>
+            <p className="mt-2 text-[#666e75] text-[14px]  ">
+              Once you created image you can
+              share with others in the community
+            </p>
+            <button
+              type="button"
+              onClick={handleSubmit}
+              className="mt-3 text-white bg-[#6369ff] font-medium rounded-md text-sm
+                   w-full sm:w-auto px-5 py-2.5 text-center"
+            >
+              {loading ? "Sharing..." : "Share with Community"}
+
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default CreatePost
